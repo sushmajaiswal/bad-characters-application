03e9ce1990631b78a6ff5d7f59a9d76b25763973
sushmajaiswal <88885347+sushmajaiswal@users.noreply.github.com>
2021-08-14 17:08:08 +0530

    Update Routes.js
    check

--- src/Routes.js
+++ src/Routes.js
@@ -5,10 +5,11 @@ import Home from "./pages/Home";
 import Character from "./pages/character";
 
 const Routes = () => {
+  console.log(window.location.href);
   return (
     <BrowserRouter>
       <Switch> 
-        <Route path="/" exact component={Home} />
+        <Route path="/p" exact component={Home} />
         <Route path="/character" component={Character} />
            <Route path="/test" component={Home} />
       </Switch>
